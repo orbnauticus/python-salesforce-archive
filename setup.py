78cffd6b80ba7d9310af0822557657dc21dd42fe
Ryan Marquardt <ryan.marquardt@gmail.com>
2016-01-02 11:06:14 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sf_archive',
+    version='0.0.1',
+    author='Ryan Marquardt',
+    author_email='ryan@integralws.com',
+    description='Unpack ',
+    license='Simplified BSD',
+    packages=['sf_archive'],
+)
